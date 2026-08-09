1c9305001810538c32fbaf4ef9bb0eac286a8489
Doktorlich <sergei199522@mail.ru>
2026-08-09 15:12:30 +0300

    style(ui): implement styling for Select

--- apps/web/src/components/ui/Select/Select.tsx
+++ apps/web/src/components/ui/Select/Select.tsx
@@ -1,4 +1,6 @@
 import { ComponentPropsWithoutRef } from "react";
+import clsx from "clsx";
+import classes from "./Select.module.scss";
 
 export interface SelectOption {
     value: string;
@@ -9,9 +11,10 @@ interface SelectProps extends ComponentPropsWithoutRef<"select"> {
     options: SelectOption[];
 }
 
-export default function Select({ options, ...props }: SelectProps) {
+export default function Select({ className,options, ...props }: SelectProps) {
+    const selectClassName = clsx(classes.select, className);
     return (
-        <select {...props}>
+        <select {...props} className={selectClassName}>
             {options.map(opt => {
                 return (
                     <option key={opt.value} value={opt.value}>
