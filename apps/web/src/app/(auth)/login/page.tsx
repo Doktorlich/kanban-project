0d45b0098d6d951b13508bed7d60c5e1f7de76bb
Doktorlich <sergei199522@mail.ru>
2026-08-19 18:02:31 +0300

    style(auth):implement styling for login and register page

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,33 +1,54 @@
 import InputLabel from "@/components/ui/InputLabel/InputLabel";
 import Button from "@/components/ui/Button/Button";
 import Link from "next/link";
+import classes from "../auth-form.module.scss";
 
 export default function LoginPage() {
     return (
-        <div className={"container"}>
-            <h1> Login</h1>
-            <p>welcome back to the platform</p>
-            <form className={"form-login"}>
-                <InputLabel type={"email"} id={"email"} placeholder={"user@example.com"}>
-                    email address
-                </InputLabel>
+        <div className={classes.container}>
+            <div className={classes.page}>
+                <h1 className={classes["page__h1"]}> Login</h1>
+                <p className={classes["page__greeting"]}>Welcome back to the platform</p>
+                <form className={classes["page__form"]}>
+                    <InputLabel
+                        type={"email"}
+                        id={"email"}
+                        placeholder={"user@example.com"}
+                        labelClassName={classes["page__input-text--label"]}
+                        className={classes["page__input-text"]}
+                    >
+                        Email address
+                    </InputLabel>
 
-                <InputLabel type={"password"} id={"password"} placeholder={"********"}>
-                    password
-                </InputLabel>
+                    <InputLabel
+                        type={"password"}
+                        id={"password"}
+                        placeholder={"********"}
+                        labelClassName={classes["page__input-text--label"]}
+                        className={classes["page__input-text"]}
+                    >
+                        Password
+                    </InputLabel>
 
-                <InputLabel type={"checkbox"} id={"remember-me"} variant={"checkbox"}>
-                    remember me
-                </InputLabel>
-                <Link href={"/"}>forgot password?</Link>
+                    <InputLabel
+                        type={"checkbox"}
+                        id={"remember-me"}
+                        variant={"checkbox"}
+                        className={classes["page__input-checkbox"]}
+                        labelClassName={classes["page__input-checkbox--label"]}
+                    >
+                        Remember me
+                    </InputLabel>
+                    <Link href={"/"} className={classes["page__restore-pass"]}>
+                        Forgot password?
+                    </Link>
 
-                <Button type={"submit"} className={"button--primary"}>
-                    login
-                </Button>
-                <p>
-                    Don&#39;t have an account? <Link href={"/register"}>Register</Link>
-                </p>
-            </form>
+                    <Button type={"submit"}>login</Button>
+                    <p className={classes["page__auth-switch"]}>
+                        Don&#39;t have an account? <Link href={"/register"}>Register</Link>
+                    </p>
+                </form>
+            </div>
         </div>
     );
 }
--- apps/web/src/app/(auth)/register/page.tsx
+++ apps/web/src/app/(auth)/register/page.tsx
@@ -1,39 +1,71 @@
 import InputLabel from "@/components/ui/InputLabel/InputLabel";
 import Button from "@/components/ui/Button/Button";
 import Link from "next/link";
-
+import classes from "../auth-form.module.scss";
 export default function RegisterPage() {
-  return (
-    <div className={"container"}>
-      <h1>create account</h1>
-      <p>get started with your free account today.</p>
-      <form className={"form-register"}>
-        <InputLabel type={"text"} id={"full-name"} placeholder={"John Doe"}>
-          full name
-        </InputLabel>
+    return (
+        <div className={classes.container}>
+            <div className={classes.page}>
+                <h1 className={classes["page__h1"]}>create account</h1>
+                <p className={classes["page__greeting"]}>Get started with your free account today.</p>
+                <form className={classes["page__form"]}>
+                    <InputLabel
+                        type={"text"}
+                        id={"full-name"}
+                        placeholder={"John Doe"}
+                        labelClassName={classes["page__input-text--label"]}
+                        className={classes["page__input-text"]}
+                    >
+                        Full name
+                    </InputLabel>
 
-        <InputLabel type={"email"} id={"email"} placeholder={"user@example.com"}>
-          email address
-        </InputLabel>
+                    <InputLabel
+                        type={"email"}
+                        id={"email"}
+                        placeholder={"user@example.com"}
+                        labelClassName={classes["page__input-text--label"]}
+                        className={classes["page__input-text"]}
+                    >
+                        Email address
+                    </InputLabel>
 
-        <InputLabel type={"password"} id={"password"} placeholder={"********"}>
-          password
-        </InputLabel>
+                    <InputLabel
+                        type={"password"}
+                        id={"password"}
+                        placeholder={"********"}
+                        labelClassName={classes["page__input-text--label"]}
+                        className={classes["page__input-text"]}
+                    >
+                        Password
+                    </InputLabel>
 
-        <InputLabel type={"password"} id={"confirm-password"} placeholder={"********"}>
-          confirm password
-        </InputLabel>
+                    <InputLabel
+                        type={"password"}
+                        id={"confirm-password"}
+                        placeholder={"********"}
+                        labelClassName={classes["page__input-text--label"]}
+                        className={classes["page__input-text"]}
+                    >
+                        Confirm password
+                    </InputLabel>
 
-        <InputLabel type={"checkbox"} id={"privacy-policy"} variant={"checkbox"} labelClassName={"label"}>
-          I agree to the <Link href={""}>Terms</Link> and <Link href={""}>Privacy Policy</Link>
-        </InputLabel>
+                    <InputLabel
+                        type={"checkbox"}
+                        id={"privacy-policy"}
+                        variant={"checkbox"}
+                        className={classes["page__input-checkbox"]}
+                        labelClassName={classes["page__input-checkbox--label-privacy"]}
+                    >
+                        I agree to the <Link href={""}>Terms</Link> and <Link href={""}>Privacy Policy</Link>
+                    </InputLabel>
 
-        <Button type={"submit"}>Create Account</Button>
+                    <Button type={"submit"}>Create Account</Button>
 
-        <p>
-          Already have an account? <Link href={"/login"}>Login</Link>
-        </p>
-      </form>
-    </div>
-  );
+                    <p className={classes["page__auth-switch"]}>
+                        Already have an account? <Link href={"/login"}>Login</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
 }
