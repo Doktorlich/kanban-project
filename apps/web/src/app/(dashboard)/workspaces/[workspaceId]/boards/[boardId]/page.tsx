df53dcfbd3af2a64d88e77baf858fc9c5c3e93d6
Doktorlich <sergei199522@mail.ru>
2026-08-20 16:25:36 +0300

    style(dashboard): implement styling for boardId

--- apps/web/src/app/(dashboard)/workspaces/[workspaceId]/boards/[boardId]/page.tsx
+++ apps/web/src/app/(dashboard)/workspaces/[workspaceId]/boards/[boardId]/page.tsx
@@ -1,53 +1,58 @@
 import Column from "@/components/board/Column/Column";
 import { Board, COLUMNS, Workspace, WORKSPACES } from "@/constants/mock-workspaces";
 import TaskCard from "@/components/task/TaskCard/TaskCard";
+import classes from "./page.module.scss";
+import clsx from "clsx";
 
 interface BoardsProps {
     params: Promise<{ workspaceId: string; boardId: string }>;
 }
 
 export default async function BoardsPage({ params }: BoardsProps) {
-    // В Next.js params является асинхронным объектом
     const { workspaceId, boardId } = await params;
     const workspace: Workspace | undefined = WORKSPACES.find(workspace => workspace.id === +workspaceId);
     const board: Board | undefined = workspace?.boards.find(board => board.id === +boardId);
+
     if (!board) {
-        return <div className="workspace">Board not found</div>;
+        return <div className={classes["workspace"]}>Board not found</div>;
     }
+    const columnItemClassName = clsx(classes["board-column__item"], classes["board-column__item--empty"]);
     return (
-        <div className="board-page">
-            <header className="board-page__header">
-                <div className="board-page__title-wrapper">
-                    <h1 className="board-page__title">Board: {board.nameBoard}</h1>
+        <div className={classes["board-page"]}>
+            <header className={classes["board-page__header"]}>
+                <div className={classes["board-page__title-wrapper"]}>
+                    <h1 className={classes["board-page__title"]}>Board: {board.nameBoard}</h1>
                 </div>
             </header>
-            <div>
+            <ul className={classes["column__list"]}>
                 {COLUMNS.map(col => {
                     const filteredTasks = board?.tasks.filter(task => task.status === col.id);
 
                     return (
-                        <Column key={col.id} status={col.title} countTasks={filteredTasks?.length}>
-                            <ul className="board-column__list">
-                                {filteredTasks?.length === 0 ? (
-                                    <li className="board-column__item board-column__item--empty">
-                                        <p className="board-column__empty-text">No tasks</p>
-                                    </li>
-                                ) : (
-                                    filteredTasks?.map(task => {
-                                        return (
-                                            <TaskCard
-                                                key={task.id}
-                                                task={task}
-                                                href={`/workspaces/${workspaceId}/boards/${boardId}/tasks/${task.id}`}
-                                            />
-                                        );
-                                    })
-                                )}
-                            </ul>
-                        </Column>
+                        <li key={col.id} className={classes["column__item"]}>
+                            <Column status={col.title} countTasks={filteredTasks?.length}>
+                                <ul className={classes["board-column__list"]}>
+                                    {filteredTasks?.length === 0 ? (
+                                        <li className={columnItemClassName}>
+                                            <p className={classes["board-column__empty-text"]}>No tasks</p>
+                                        </li>
+                                    ) : (
+                                        filteredTasks?.map(task => {
+                                            return (
+                                                <TaskCard
+                                                    key={task.id}
+                                                    task={task}
+                                                    href={`/workspaces/${workspaceId}/boards/${boardId}/tasks/${task.id}`}
+                                                />
+                                            );
+                                        })
+                                    )}
+                                </ul>
+                            </Column>
+                        </li>
                     );
                 })}
-            </div>
+            </ul>
         </div>
     );
 }
