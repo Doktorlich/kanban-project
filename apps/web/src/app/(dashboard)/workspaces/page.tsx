9b52e47fb8fe3b014833045f178b0a44c24cc6e8
Doktorlich <sergei199522@mail.ru>
2026-08-20 15:54:45 +0300

    style(dashboard): implement styling for workspaces page

--- apps/web/src/app/(dashboard)/workspaces/page.tsx
+++ apps/web/src/app/(dashboard)/workspaces/page.tsx
@@ -1,31 +1,34 @@
 import Button from "@/components/ui/Button/Button";
 import WorkspaceCard from "@/components/workspace/WorkspaceCard";
 import { WORKSPACES } from "@/constants/mock-workspaces";
+import classes from "./page.module.scss";
 
 export default function WorkspacesPage() {
     return (
-        <div className={"workspaces"}>
-            <header className="workspaces-header">
-                <div className="workspaces-header__title-block">
+        <div className={classes.workspaces}>
+            <header className={classes["workspaces-header"]}>
+                <div className={classes["workspaces-header__title-block"]}>
                     <h1>Your Workspaces</h1>
                     <p>{WORKSPACES.length} workspaces</p>
                 </div>
                 {/*Данная кнопка под вопросом, т к есть элемент создания внутри карточек workspaces*/}
-                <Button type={"button"} className={"workspaces-header__add-workspace"}>
+                <Button type={"button"} className={classes["workspaces-header__add-workspace"]}>
                     + New workspace
                 </Button>
             </header>
 
-            <section className={"workspaces-cards"}>
-                <ul className={"workspaces-cards__list"}>
+            <section className={classes["workspaces-cards"]}>
+                <ul className={classes["workspaces-cards__list"]}>
                     {WORKSPACES.map(card => (
                         <WorkspaceCard card={card} key={card.id} href={`/workspaces/${card.id}`} />
                     ))}
                 </ul>
 
-                <Button type={"button"} className={"workspaces-cards__add-workspace"}>
+                <Button type={"button"} className={classes["workspaces-cards__add-workspace"]}>
                     <span>+</span>
-                    <p>Create workspace</p>
+                    <p>
+                        Create <br /> workspace
+                    </p>
                 </Button>
             </section>
         </div>
